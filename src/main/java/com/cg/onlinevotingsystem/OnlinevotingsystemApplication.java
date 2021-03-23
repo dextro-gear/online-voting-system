5314a6ef134b7bb4281cbeea68d20c23f129e034
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-23 22:14:36 +0530

    Initialize Project

---
+++ src/main/java/com/cg/onlinevotingsystem/OnlinevotingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.cg.onlinevotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinevotingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinevotingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/onlinevotingsystem/OnlinevotingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.onlinevotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinevotingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
