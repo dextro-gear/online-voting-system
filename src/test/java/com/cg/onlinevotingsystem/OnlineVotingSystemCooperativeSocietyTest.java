cb6db60d8f1c1ce46f243cf0ffcdf9961502c5d0
s-nisha <nishi30599@gmail.com>
2021-03-25 00:27:18 +0530

    test edited

--- src/test/java/com/cg/onlinevotingsystem/OnlineVotingSystemCooperativeSocietyTest.java
+++ src/test/java/com/cg/onlinevotingsystem/OnlineVotingSystemCooperativeSocietyTest.java
@@ -1,30 +1,22 @@
 package com.cg.onlinevotingsystem;
 
-import static org.mockito.Mockito.when;
-
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.cg.onlinevotingsystem.cooperativesocietyms.entities.CooperativeSociety;
-import com.cg.onlinevotingsystem.cooperativesocietyms.service.ICooperativeSocietyRepository;
 import com.cg.onlinevotingsystem.cooperativesocietyms.service.ICooperativeSocietyService;
 
 @SpringBootTest
 class OnlineVotingSystemCooperativeSocietyTest {
 	
 	@Autowired
-	 ICooperativeSocietyService service;
-	@MockBean
-	ICooperativeSocietyRepository repository;
+
 	
 	@Test
-	public void ViewSocietyListTest( ) {
+	public void ViewSocietyByID( ) {
 		CooperativeSociety s1 =new CooperativeSociety() ;
 		s1.setSocietyId(1);
 		s1.setHeadOfSociety("Divena");
@@ -34,13 +26,20 @@ class OnlineVotingSystemCooperativeSocietyTest {
 		s1.setPincode("603203");
 		s1.setDistrict("keni");
 		
-		s1 = service.viewSocietyById(1);
+
 		
 		 
 		
 		Assertions.assertEquals(1,s1.getSocietyId() );
 		Assertions.assertEquals("Divena",s1.getHeadOfSociety())	;
 		
+
+	}
+	@Test
+	public void ViewSocietyList(){
+		CooperativeSociety society1 = new CooperativeSociety(1,"migan","miesha","sursuri","gantantra","kahar","480608");
+		CooperativeSociety society2 = new CooperativeSociety(2,"Meraki","mahira","devgarh","janana","Kaman","610401");
+
 
 	}
 
