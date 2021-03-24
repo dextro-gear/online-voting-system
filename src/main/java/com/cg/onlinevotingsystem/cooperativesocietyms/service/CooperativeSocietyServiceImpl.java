adff6462826197522d354ed013c019d7a42fbaf7
s-nisha <nishi30599@gmail.com>
2021-03-24 16:26:21 +0530

    created service class for CooperativeSociety

---
+++ src/main/java/com/cg/onlinevotingsystem/cooperativesocietyms/service/CooperativeSocietyServiceImpl.java
@@ -0,0 +1,39 @@
+package com.cg.onlinevotingsystem.cooperativesocietyms.service;
+
+import java.util.List;
+
+import com.cg.onlinevotingsystem.cooperativesocietyms.entities.CooperativeSociety;
+
+public class CooperativeSocietyServiceImpl implements ICooperativeSocietyService {
+
+	@Override
+	public int addSocietyDetails(CooperativeSociety society) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateSocietyDetails(CooperativeSociety society) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteSociety(int societyId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<CooperativeSociety> viewSocietyList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CooperativeSociety viewSocietyById(int societyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/cg/onlinevotingsystem/cooperativesocietyms/service/ICooperativeSocietyRepository.java
@@ -0,0 +1,9 @@
+package com.cg.onlinevotingsystem.cooperativesocietyms.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.onlinevotingsystem.cooperativesocietyms.entities.CooperativeSociety;
+
+public interface ICooperativeSocietyRepository extends JpaRepository<CooperativeSociety, Integer> {
+
+}
