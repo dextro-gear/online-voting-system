c64e8d00d6476eece87f20ad5c76b3feb1baf130
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-25 12:20:21 +0530

    Implement Service Methods

--- src/main/java/com/cg/onlinevotingsystem/votedlistms/services/VotedListServiceImpl.java
+++ src/main/java/com/cg/onlinevotingsystem/votedlistms/services/VotedListServiceImpl.java
@@ -1,27 +1,52 @@
 package com.cg.onlinevotingsystem.votedlistms.services;
 
+import com.cg.onlinevotingsystem.cooperativesocietyms.entities.CooperativeSociety;
+import com.cg.onlinevotingsystem.nominatedcandidatems.entities.NominatedCandidates;
+import com.cg.onlinevotingsystem.votedlistms.dao.VotedListRepository;
 import com.cg.onlinevotingsystem.votedlistms.entities.VotedList;
+import com.cg.onlinevotingsystem.voterms.entities.RegisteredSocietyVoters;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class VotedListServiceImpl implements VotedListService {
+
+    @Autowired
+    VotedListRepository votedListRepository;
+
     @Override
-    public int castVotedList(VotedList votedList) {
-        return 0;
+    public VotedList castVotedList(RegisteredSocietyVoters voter, NominatedCandidates candidate, CooperativeSociety society) {
+        VotedList vote = new VotedList(voter, candidate, society);
+        return votedListRepository.save(vote);
     }
 
     @Override
-    public int updateVotedListDetails(VotedList votedList) {
-        return 0;
+    // this method will update the detail in the record
+    public VotedList updateVotedListDetails(int votedListID, RegisteredSocietyVoters voter, NominatedCandidates candidate, CooperativeSociety society) {
+        return null;
     }
 
     @Override
-    public int deletedVotedListDetails(int id) {
-        return 0;
+    public VotedList deletedVotedListDetails(int id) {
+        // find the VotedList
+        // delete
+        return null;
     }
 
     @Override
     public List<VotedList> viewVotedList() {
+        return null;
+    }
+
+
+    @Override
+    public VotedList searchByVoterId(int voterId) {
+        return null;
+    }
+
+    @Override
+    public List<VotedList> searchByNominatedCandidateId(int candidateId) {
         return null;
     }
 }
---
+++ src/main/java/com/cg/onlinevotingsystem/votedlistms/ui/VotedListUI.java
@@ -0,0 +1,17 @@
+package com.cg.onlinevotingsystem.votedlistms.ui;
+
+import com.cg.onlinevotingsystem.votedlistms.entities.VotedList;
+import com.cg.onlinevotingsystem.votedlistms.services.VotedListServiceImpl;
+import com.cg.onlinevotingsystem.voterms.entities.RegisteredSocietyVoters;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VotedListUI {
+
+    @Autowired
+    VotedListServiceImpl votedListService;
+
+    void start(){
+
+    }
+
+}
