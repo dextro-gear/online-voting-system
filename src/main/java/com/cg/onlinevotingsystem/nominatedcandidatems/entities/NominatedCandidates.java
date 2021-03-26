36f87bbed2fe23a74765f4443fe27b6266fe53bd
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-26 12:05:12 +0530

    Implement NominatedCandidates service

--- src/main/java/com/cg/onlinevotingsystem/nominatedcandidatems/entities/NominatedCandidates.java
+++ src/main/java/com/cg/onlinevotingsystem/nominatedcandidatems/entities/NominatedCandidates.java
@@ -5,6 +5,8 @@ import com.cg.onlinevotingsystem.voterms.entities.RegisteredSocietyVoters;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 @Entity
 public class NominatedCandidates{
 
@@ -14,14 +16,16 @@ public class NominatedCandidates{
 
     private String nominationFormNo;
 
-//    RegisteredSocietyVoters societyVoter;
+    @OneToOne
+    RegisteredSocietyVoters societyVoter;
 
 
     public NominatedCandidates() {
     }
 
-    public NominatedCandidates(String nominationFormNo) {
+    public NominatedCandidates(String nominationFormNo, RegisteredSocietyVoters voter) {
         this.nominationFormNo = nominationFormNo;
+        this.societyVoter = voter;
     }
 
     public int getCandidateID() {
@@ -34,5 +38,13 @@ public class NominatedCandidates{
 
     public void setNominationFormNo(String nominationFormNo) {
         this.nominationFormNo = nominationFormNo;
+    }
+
+    public RegisteredSocietyVoters getSocietyVoter() {
+        return societyVoter;
+    }
+
+    public void setSocietyVoter(RegisteredSocietyVoters societyVoter) {
+        this.societyVoter = societyVoter;
     }
 }
--- src/main/java/com/cg/onlinevotingsystem/nominatedcandidatems/services/NominatedCandidateServiceImpl.java
+++ src/main/java/com/cg/onlinevotingsystem/nominatedcandidatems/services/NominatedCandidateServiceImpl.java
@@ -1,11 +1,14 @@
 package com.cg.onlinevotingsystem.nominatedcandidatems.services;
 
+import com.cg.onlinevotingsystem.dashboard.exceptions.CandidateNotFoundException;
 import com.cg.onlinevotingsystem.nominatedcandidatems.dao.INominatedCandidateRepository;
 import com.cg.onlinevotingsystem.nominatedcandidatems.entities.NominatedCandidates;
 import com.cg.onlinevotingsystem.voterms.entities.RegisteredSocietyVoters;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 public class NominatedCandidateServiceImpl implements INominatedCandidatesService{
 
@@ -13,27 +16,45 @@ public class NominatedCandidateServiceImpl implements INominatedCandidatesServic
     INominatedCandidateRepository nominatedCandidateRepository;
 
     @Override
-    public NominatedCandidates addNominatedCandidate(String nominationFormNo, RegisteredSocietyVoters societyVoters) {
-        return null;
+    @Transactional
+    public NominatedCandidates addNominatedCandidate(String nominationFormNo, RegisteredSocietyVoters societyVoter) {
+        return nominatedCandidateRepository.save(new NominatedCandidates(nominationFormNo, societyVoter));
     }
 
     @Override
-    public NominatedCandidates updateNominatedCandidateDetails(String nominationFormNo, RegisteredSocietyVoters societyVoters) {
-        return null;
+    @Transactional
+    public NominatedCandidates updateNominatedCandidateDetails(int id, String nominationFormNo, RegisteredSocietyVoters societyVoter) {
+        Optional<NominatedCandidates> candidatesOptional = nominatedCandidateRepository.findById(id);
+        NominatedCandidates candidate = candidatesOptional.get();
+        if(candidatesOptional.isPresent()){
+            candidate.setNominationFormNo(nominationFormNo);
+            candidate.setSocietyVoter(societyVoter);
+        }else
+            throw new CandidateNotFoundException("Candidate with id" + id + " not found in the DB");
+
+        return nominatedCandidateRepository.save(candidate);
     }
 
     @Override
     public NominatedCandidates deleteNominatedCandidate(int candidateID) {
-        return null;
+        Optional<NominatedCandidates> candidatesOptional = nominatedCandidateRepository.findById(candidateID);
+        if(!candidatesOptional.isPresent())
+            throw new CandidateNotFoundException("Candidate with id" + candidateID + " not found in the DB");
+        else
+            nominatedCandidateRepository.delete(candidatesOptional.get());
+        return candidatesOptional.get();
     }
 
     @Override
     public NominatedCandidates searchByCandidateID(int candidateID) {
-        return null;
+        Optional<NominatedCandidates> candidatesOptional = nominatedCandidateRepository.findById(candidateID);
+        if(candidatesOptional.isPresent())
+            throw new CandidateNotFoundException("Candidate with id" + candidateID + " not found in the DB");
+        return candidatesOptional.get();
     }
 
     @Override
     public List<NominatedCandidates> viewNominatedCandidateList() {
-        return null;
+        return nominatedCandidateRepository.findAll();
     }
 }
