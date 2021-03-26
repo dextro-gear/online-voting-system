a61f5b5fdffb55689f2a632bb45ab550b5106195
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-26 14:31:21 +0530

    Update method implementations

--- src/main/java/com/cg/onlinevotingsystem/votedlistms/dao/IVotedListRepository.java
+++ src/main/java/com/cg/onlinevotingsystem/votedlistms/dao/IVotedListRepository.java
@@ -1,7 +1,13 @@
 package com.cg.onlinevotingsystem.votedlistms.dao;
 
+import com.cg.onlinevotingsystem.nominatedcandidatems.entities.NominatedCandidates;
 import com.cg.onlinevotingsystem.votedlistms.entities.VotedList;
+import com.cg.onlinevotingsystem.voterms.entities.RegisteredSocietyVoters;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IVotedListRepository extends JpaRepository<VotedList, Integer> {
+    VotedList findByIdVoter(RegisteredSocietyVoters voter);
+    List<VotedList> findByIdCandidate(NominatedCandidates candidates);
 }
