cf23b64c92d62aa43b3cd8d9e6839947f67eca27
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-27 20:52:39 +0530

    Add REST controller for NominatedController

---
+++ src/main/java/com/cg/onlinevotingsystem/nominatedcandidatems/controllers/NominatedCandidateRESTController.java
@@ -0,0 +1,17 @@
+package com.cg.onlinevotingsystem.nominatedcandidatems.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NominatedCandidateRESTController {
+
+    /* TODO
+        Nominated Candidate REST Endpoints
+    *   1. Add candidate
+    *   2. Update candidate
+    *   3. Delete candidate
+    *   4. View candidate by ID
+    *   5. View all candidates
+    * */
+
+}
